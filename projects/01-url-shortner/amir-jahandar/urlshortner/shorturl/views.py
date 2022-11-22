b27c89cf18987509a5a395167795df919b801388
amirjahandar <amirjahanwz@gmail.com>
2022-11-22 22:24:34 +0330

    add digits in shorturl

--- projects/01-url-shortner/amir-jahandar/urlshortner/shorturl/views.py
+++ projects/01-url-shortner/amir-jahandar/urlshortner/shorturl/views.py
@@ -21,20 +21,22 @@ def createshorturl(request):
         if form.is_valid():
             originalurl = form.cleaned_data['original_url']
             random_chars_list = list(string.ascii_letters)
+            random_digits_list = [str(i) for i in range(10)]
             random_chars = ''
-            for i in range(6):
+            for i in range(3):
                 random_chars += random.choice(random_chars_list)
+                random_chars += random.choice(random_digits_list)
             while len(ShortUrl.objects.filter(short_url=random_chars)) != 0:
-                for i in range(6):
+                for i in range(2):
                     random_chars += random.choice(random_chars_list)
+                    random_chars += random.choice(random_digits_list)
             d = datetime.now()
             if request.user.is_authenticated:
                 
                 s = ShortUrl(original_url=originalurl, short_url=random_chars, time_date_created=d, profile= request.user.profile)
                 s.save()
                 return redirect(originalurl)
-            return render(request, 'login.html')
-        
+            return render(request, 'login.html')        
 
     else:
         form = CreateNewShortUrl()
@@ -91,3 +93,6 @@ def logout(request):
         return render(request, 'home.html', {'msg':'See you later'})
 
     return render(request, 'home.html')
+
+
+
